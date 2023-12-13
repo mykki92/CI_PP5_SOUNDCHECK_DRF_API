f65293b047b329659cd956f60c00661db4c84406
mykki92 <mike2108@hotmail.co.uk>
2023-12-13 19:42:46 +0000

    Include profile serializer in the ProfileList view

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
 
 
 class ProfileList(APIView):
@@ -10,4 +11,5 @@ class ProfileList(APIView):
     """
     def get(self, request):
         profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
