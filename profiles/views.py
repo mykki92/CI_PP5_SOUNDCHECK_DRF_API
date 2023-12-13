c58b4e0e85b0a6d9df95160096dade478f1f1d26
mykki92 <mike2108@hotmail.co.uk>
2023-12-13 20:11:53 +0000

    Define put function in ProfileDetail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -17,6 +18,8 @@ class ProfileList(APIView):
 
 
 class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+    
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
@@ -29,3 +32,13 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
