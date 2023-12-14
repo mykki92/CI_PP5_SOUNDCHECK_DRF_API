c7fb02d41b0bb56692f871717fb06f823f2bf6d4
mykki92 <mike2108@hotmail.co.uk>
2023-12-14 17:12:26 +0000

    Define the post function in the post list view to deserialize the post data

--- posts/views.py
+++ posts/views.py
@@ -6,9 +6,24 @@ from .serializers import PostSerializer
 
 
 class PostList(APIView):
+    serializer_class = PostSerializer
+
     def get(self, request):
         posts = Post.objects.all()
         serializer = PostSerializer(
             posts, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
+    def post(self, request):
+            serializer = PostSerializer(
+                data=request.data, context={'request': request}
+            )
+            if serializer.is_valid():
+                serializer.save(owner=request.user)
+                return Response(
+                    serializer.data, status=status.HTTP_201_CREATED
+                )
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
