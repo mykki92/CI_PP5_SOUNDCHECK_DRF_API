e2a855fe4742474a18ae1f9cb7efdb7db6cb4163
mykki92 <mike2108@hotmail.co.uk>
2023-12-14 17:32:44 +0000

    Create post detail view with get object and get functions

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
+from soundcheck_drf_api.permissions import IsOwnerOrReadOnly
 
 
 class PostList(APIView):
@@ -36,3 +38,23 @@ class PostList(APIView):
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class PostDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
