cb661d8bb8ca6089d15db4ca13ad666d90ea7ce9
mykki92 <mike2108@hotmail.co.uk>
2023-12-14 17:22:46 +0000

    Add comments to posts/models.py

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    """
+    A class for the post model
+    """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
