bc9f0bcf858887e92545aa6792cacd0e8d3bf991
mykki92 <mike2108@hotmail.co.uk>
2023-12-14 17:36:55 +0000

    Define the put function in post detail view to enable post editing

--- posts/views.py
+++ posts/views.py
@@ -58,3 +58,15 @@ class PostDetail(APIView):
             post, context={'request': request}
         )
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
