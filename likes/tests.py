1dd6d36787f1fb40ff0dc0553fe2e0182ab734a3
mykki92 <mike2108@hotmail.co.uk>
2023-12-15 16:44:43 +0000

    Create test class for like list and like detail views

--- likes/tests.py
+++ likes/tests.py
@@ -1,2 +1,59 @@
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Like
+from posts.models import Post
 
 
+class LikeListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='tom', password='password')
+
+    def test_not_logged_in_user_cannot_like_post(self):
+        response = self.client.post('/likes/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class LikeDetailViewTests(APITestCase):
+    def setUp(self):
+        tom = User.objects.create_user(username='tom', password='password')
+        jerry = User.objects.create_user(username='jerry', password='password')
+        Post.objects.create(
+            owner=tom, title='post title',
+            content='test'
+        )
+        Post.objects.create(
+            owner=jerry, title='post title2',
+            content='test2'
+        )
+        Post.objects.create(
+            owner=jerry, title='post title3',
+            content='test3'
+        )
+        Like.objects.create(owner=tom, post_id=2)
+        Like.objects.create(owner=jerry, post_id=1)
+
+    def test_logged_in_user_can_like_post(self):
+        self.client.login(username='tom', password='password')
+        response = self.client.post('/likes/', {'post': 3})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_can_retrieve_existing_like(self):
+        self.client.login(username='tom', password='password')
+        response = self.client.get('/likes/1/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cannot_retrieve_non_existing_like(self):
+        self.client.login(username='tom', password='password')
+        response = self.client.get('/likes/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_unlike_own_like(self):
+        self.client.login(username='tom', password='password')
+        response = self.client.delete('/likes/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_user_can_unlike_other_user_like(self):
+        self.client.login(username='tom', password='password')
+        response = self.client.delete('/likes/2/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
