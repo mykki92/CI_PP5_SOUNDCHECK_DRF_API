8a8e77726cab0ac5666d01a2a26ddff5fcc8145a
mykki92 <mike2108@hotmail.co.uk>
2024-01-31 22:52:14 +0000

    Create attending list view

--- attending/views.py
+++ attending/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from .models import Attending
+from .serializers import AttendingSerializer
+
+
+
+class AttendingListView(generics.ListCreateAPIView):
+    """
+    List view of all events the user is attending
+    """
+    serializer_class = AttendingSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Join.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['event']
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
