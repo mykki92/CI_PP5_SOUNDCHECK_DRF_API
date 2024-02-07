93d80feca0c49b5af76f5f0cd59fd0d304bee571
mykki92 <mike2108@hotmail.co.uk>
2024-02-07 13:55:26 +0000

    Update likes views

--- likes/tests.py
+++ likes/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Like
+from .models import Check
 from posts.models import Post
 
 
--- likes/views.py
+++ likes/views.py
@@ -1,25 +1,25 @@
 from rest_framework import generics, permissions
 from soundcheck_drf_api.permissions import IsOwnerOrReadOnly
-from likes.models import Like
-from likes.serializers import LikeSerializer
+from likes.models import Check
+from likes.serializers import CheckSerializer
 
 
-class LikeList(generics.ListCreateAPIView):
+class CheckList(generics.ListCreateAPIView):
     """
     A class to list likes or create a like if logged in.
     """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    serializer_class = LikeSerializer
-    queryset = Like.objects.all()
+    serializer_class = CheckSerializer
+    queryset = Check.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class LikeDetail(generics.RetrieveDestroyAPIView):
+class CheckDetail(generics.RetrieveDestroyAPIView):
     """
     Retrieve or delete  a like by id.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = LikeSerializer
-    queryset = Like.objects.all()
+    serializer_class = CheckSerializer
+    queryset = Check.objects.all()
